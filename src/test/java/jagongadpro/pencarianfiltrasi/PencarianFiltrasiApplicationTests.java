755c834590f04712d4e4be55e873aa714a07ab97
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-04-17 07:31:32 +0700

    Initialize spring boot

---
+++ src/test/java/jagongadpro/pencarianfiltrasi/PencarianFiltrasiApplicationTests.java
@@ -0,0 +1,13 @@
+package jagongadpro.pencarianfiltrasi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PencarianFiltrasiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
