b77fdbc88e31d74d2eeda862b976747a1d1fcaf6
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-04-24 16:34:06 +0700

    [RED] Add skeleton for Game model

---
+++ src/main/java/jagongadpro/pencarianfiltrasi/model/Game.java
@@ -0,0 +1,10 @@
+package jagongadpro.pencarianfiltrasi.model;
+
+public class Game {
+    private String id;
+    private String nama;
+    private String deskripsi;
+    private Integer harga;
+    private String kategori;
+    private Integer stok;
+}
