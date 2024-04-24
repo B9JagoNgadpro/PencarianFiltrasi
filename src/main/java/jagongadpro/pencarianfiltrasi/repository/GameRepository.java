a651c61752911ff41446158db9ced93d552f2827
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-04-24 19:23:56 +0700

    [FIX] Align Game model ID type with Repository and fix query method names

--- src/main/java/jagongadpro/pencarianfiltrasi/repository/GameRepository.java
+++ src/main/java/jagongadpro/pencarianfiltrasi/repository/GameRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, Long> {
-    List<Game> findByNameContaining(String name);
+    List<Game> findByNameContaining(String nama);
 }
