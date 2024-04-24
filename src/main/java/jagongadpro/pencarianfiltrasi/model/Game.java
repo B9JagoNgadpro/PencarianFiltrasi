0bf8233996f120f3533467278aa31160af2cad7d
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-04-24 16:36:29 +0700

    [GREEN] Implement Game model

--- src/main/java/jagongadpro/pencarianfiltrasi/model/Game.java
+++ src/main/java/jagongadpro/pencarianfiltrasi/model/Game.java
@@ -1,10 +1,20 @@
 package jagongadpro.pencarianfiltrasi.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Game {
-    private String id;
-    private String nama;
-    private String deskripsi;
-    private Integer harga;
-    private String kategori;
-    private Integer stok;
+    @Id
+    String id;
+    String nama;
+    String deskripsi;
+    Integer harga;
+    String kategori;
+    Integer stok;
 }
