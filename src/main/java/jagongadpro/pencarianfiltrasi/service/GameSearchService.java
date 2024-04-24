4053e70c63410d6ab3e5843769d189ce07bb46f4
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-04-24 20:08:00 +0700

    [RED] Define GameSearchService interface to abstract game search operations

--- src/main/java/jagongadpro/pencarianfiltrasi/service/GameSearchService.java
+++ src/main/java/jagongadpro/pencarianfiltrasi/service/GameSearchService.java
@@ -1,23 +1,8 @@
 package jagongadpro.pencarianfiltrasi.service;
 
 import jagongadpro.pencarianfiltrasi.model.Game;
-import jagongadpro.pencarianfiltrasi.repository.GameRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 import java.util.List;
 
-@Service
-public class GameSearchService {
-
-    private final GameRepository gameRepository;
-
-    @Autowired
-    public GameSearchService(GameRepository gameRepository) {
-        this.gameRepository = gameRepository;
-    }
-
-    public List<Game> findGamesByName(String nama) {
-        return gameRepository.findByNameContaining(nama);
-    }
+public interface GameSearchService {
+    List<Game> findGamesByName(String nama);
 }
