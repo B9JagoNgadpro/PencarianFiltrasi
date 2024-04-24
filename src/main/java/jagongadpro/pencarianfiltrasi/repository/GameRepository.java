fc209d9f322b799b46185011afdb914efb992318
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-04-24 16:35:37 +0700

    [RED] Add skeleton for GameRepository

---
+++ src/main/java/jagongadpro/pencarianfiltrasi/repository/GameRepository.java
@@ -0,0 +1,10 @@
+package jagongadpro.pencarianfiltrasi.repository;
+
+import jagongadpro.pencarianfiltrasi.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface GameRepository extends JpaRepository<Game, Long> {
+    List<Game> findByNameContaining(String name);
+}
