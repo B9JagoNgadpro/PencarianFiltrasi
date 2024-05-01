0cff15d3bdcd7936c27a76fe3fefc83d4129765a
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-05-01 20:18:53 +0700

    [REFACTOR] Update the GameRepository to use the correct method name

--- src/main/java/jagongadpro/pencarianfiltrasi/repository/GameRepository.java
+++ src/main/java/jagongadpro/pencarianfiltrasi/repository/GameRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
-public interface GameRepository extends JpaRepository<Game, Long> {
-    List<Game> findByNameContaining(String nama);
+public interface GameRepository extends JpaRepository<Game, String> {
+    List<Game> findByNamaContaining(String nama);
 }
