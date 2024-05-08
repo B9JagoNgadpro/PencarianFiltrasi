a0f166bf54df85201baef89c9cbe5f98027244a1
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-05-08 20:43:53 +0700

    [GREEN] Implement AsyncConfig to enable asynchronous processing

---
+++ src/main/java/jagongadpro/pencarianfiltrasi/config/AsyncConfig.java
@@ -0,0 +1,24 @@
+package jagongadpro.pencarianfiltrasi.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig implements AsyncConfigurer {
+
+    @Override
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(10);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("GameLookup-");
+        executor.initialize();
+        return executor;
+    }
+}
