8bb7193e01486cb99c9d5484cdda6bc387dc1222
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-05-14 21:27:36 +0700

    [REFACTOR] Extend GameRepository to support complex queries with JpaSpecificationExecutor

--- src/main/java/jagongadpro/pencarianfiltrasi/repository/GameRepository.java
+++ src/main/java/jagongadpro/pencarianfiltrasi/repository/GameRepository.java
@@ -2,9 +2,10 @@ package jagongadpro.pencarianfiltrasi.repository;
 
 import jagongadpro.pencarianfiltrasi.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
 import java.util.List;
 
-public interface GameRepository extends JpaRepository<Game, String> {
+public interface GameRepository extends JpaRepository<Game, String>, JpaSpecificationExecutor<Game> {
     List<Game> findByNamaContaining(String nama);
 }
