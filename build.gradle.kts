a6b08459bebed5aaf2f8c3da2be52b47715a190d
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-05-14 16:08:37 +0700

    Fix local database

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,6 +25,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.postgresql:postgresql:42.6.0")
     compileOnly("org.projectlombok:lombok")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
