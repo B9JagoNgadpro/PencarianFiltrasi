a3fe98392aad7a9f6d0552633986a0f10871b461
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-05-15 01:22:25 +0700

    [REFACTOR] Implement searchGames repository

--- src/main/java/jagongadpro/pencarianfiltrasi/repository/GameRepository.java
+++ src/main/java/jagongadpro/pencarianfiltrasi/repository/GameRepository.java
@@ -3,9 +3,14 @@ package jagongadpro.pencarianfiltrasi.repository;
 import jagongadpro.pencarianfiltrasi.model.Game;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface GameRepository extends JpaRepository<Game, String>, JpaSpecificationExecutor<Game> {
     List<Game> findByNamaContaining(String nama);
+    @Query("SELECT g FROM Game g WHERE " +
+            "LOWER(g.nama) LIKE LOWER(CONCAT('%', :query, '%')) OR " +
+            "LOWER(g.deskripsi) LIKE LOWER(CONCAT('%', :query, '%'))")
+    List<Game> searchGames(String query);
 }
