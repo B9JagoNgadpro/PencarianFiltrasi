057f4d09c990b5c6dd220f56d1a5d902d377a5f6
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-05-26 02:31:14 +0700

    [CONFIG] Add new dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -39,6 +39,9 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     runtimeOnly("io.netty:netty-resolver-dns-native-macos:4.1.76.Final:osx-aarch_64")
     testImplementation("com.squareup.okhttp3:mockwebserver:4.9.3")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
 }
 
 tasks.withType<Test> {
