e83cc73c7a020c75c3cce130733a10ee1839eb9e
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-05-26 16:21:42 +0700

    [FIX] Change local port to accept front end

--- src/main/java/jagongadpro/pencarianfiltrasi/config/WebConfig.java
+++ src/main/java/jagongadpro/pencarianfiltrasi/config/WebConfig.java
@@ -11,7 +11,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+//                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3001")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
