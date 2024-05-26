e3833995a5fa0be562d0333daa42702a74ce3c98
Wahyu Hidayat <wahyuhidayaat510@gmail.com>
2024-05-26 16:34:34 +0700

    [FIX] Use actual front end url

--- src/main/java/jagongadpro/pencarianfiltrasi/config/WebConfig.java
+++ src/main/java/jagongadpro/pencarianfiltrasi/config/WebConfig.java
@@ -11,8 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000")
-                .allowedOrigins("http://localhost:3002")
+                .allowedOrigins("https://gametime-frontend.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
